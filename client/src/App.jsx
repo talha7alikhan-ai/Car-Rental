765bc46fdaf852e4c6cf6c5ef501833657d4f938
Talha <your-github-email@gmail.com>
2025-12-16 18:18:44 +0500

    Initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react'
+import Navbar from './components/Navbar.jsx'
+import { Route,Routes,useLocation } from 'react-router-dom'
+import CarDetails from './pages/CarDetails.jsx';
+import Home from './pages/Home.jsx';
+import Cars from './pages/Cars.jsx';
+import MyBookings from './pages/MyBookings.jsx';
+const App = () => {
+
+    const [ShowLogin, setShowLogin] = useState(false);
+     const isOwnerPath = useLocation().pathname.startsWith('/owner'); 
+
+
+  return (
+      <>
+      {! isOwnerPath && <Navbar setShowLogin={setShowLogin} />}
+
+        <Routes>
+             <Route path='/' element={<Home/>} />
+             <Route path='/ car-details/:id' element={<CarDetails/>} />
+             <Route path='/cars' element={<Cars/>} />
+             <Route path='/my-bookings' element={<MyBookings/>}/>
+        </Routes>
+
+     </>
+
+  
+  )
+}
+
+export default App
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Hero from '../components/Hero'
+import FeaturedSection from '../components/FeaturedSection'
+import Banner from '../components/Banner'
+import Testimonial from '../components/Testimonial'
+
+const Home = () => {
+  return (
+    <>
+      <Hero />
+      <FeaturedSection />
+      <Banner />
+      <Testimonial />
+    </>
+  )
+}
+
+export default Home
