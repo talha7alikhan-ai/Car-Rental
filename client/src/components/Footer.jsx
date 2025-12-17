778bb97f2ec724072ec2d9652c06363bfdc53779
Talha <your-github-email@gmail.com>
2025-12-17 17:17:00 +0500

    added cars page

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { assets } from '../assets/assets'
 
+
 const Footer = () => {
   return (
     <div className='px-6 md:px-16 lg:px-24 xl:px-32 mt-60 text-sm text-gray-500'>
 
-            <div className='flex flex-wrap justify-between item-start gap-8 pb-6 border-borderColor border-b'>
+            <div className='flex flex-wrap justify-between items-start gap-8 pb-6 border-borderColor border-b'>
                 <div>
                     <img src={assets.logo} alt="logo" className='h-8 md:h-9'/>
                     <p className='max-w-80 mt-3'>
@@ -49,7 +50,7 @@ const Footer = () => {
                     <ul className='mt-3 flex flex-col gap-1.5'>
                         <li>1234 Luxury Drive</li>
                         <li> San Francisco CA 94107</li>
-                        <li>+92 123 456789</li>
+                        <li>+1 123 456789</li>
                         <li>info@example.com</li>
                         
                     </ul>
--- client/src/pages/CarDetails.jsx
+++ client/src/pages/CarDetails.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
 import { assets, dummyCarData } from '../assets/assets'
+import Loader from '../components/Loader'
+
 
 const CarDetails = () => {
 
@@ -16,11 +18,62 @@ const CarDetails = () => {
     <div className='px-6 md:px-16 lg:px-24 xl:px-32 mt-16'>
 
       <button onClick={()=> navigate(-1)} className='flex items-center gap-2 mb-6 text-gray-500 cursor-pointer'>
-        <img src={assets.arrow_icon} alt="" className='rotate-180 opacity-65'/>
-        Back to all cars
+        <img src={assets.arrow_icon} alt="" className='rotate-180 opacity-65'/> 
+          Back to all cars
         </button>
+
+         <div className='grid grid-cols-1 lg:grid-cols-3 gap-8 lg:gap-12'>
+             {/*Left : Car Image & Details */}
+             <div className='lg:col-span-2'>
+                 <img src={"car.image"} alt="" className='w-full h-auto md:max-h-100 object-cover rounded-xl mb-6 shadow-md'/>
+                 <div className='space-y-6'>
+                  <div>
+                      <h1 className='text-3xl font-bold'>{car.brand} {car.model} </h1>
+                      <p> {car.category} • {car.year}</p>
+                    </div> 
+                      <hr className='border-bordercolor my-6' />
+                      <div className='grid grid-cols-2 sm:grid-cols-4 gap-4'>
+                           {[
+                             {icon: assets.users_icon, text: `${car.seating_capacity} Seats`},
+                             {icon: assets.fuel_icon, text: car.fuel_type},
+                             {icon: assets.car_icon, text: car.transmission},
+                             {icon: assets.location_icon, text: car.location},
+                           ].map(({icon, text})=>(
+                             <div key={text} className='flex flex-col items-center bg-light p-4 rounded-lg'>
+                               <img src={icon} alt="" className='h-5 mb-2'/>
+                               {text}
+                             </div>
+                           ))}
+                        </div> 
+
+                        {/* Description */}
+                        <div>
+                          <h1 className='text-xl font-medium mb-3'>Description</h1>
+                          <p className='text-gray-500'>car.description</p>
+                        </div>
+                        {/* Features */}
+                        <div>
+                          <h1 className='text-xl font-medium mb-3'>Features</h1>
+                         <ul className='grid grid-cols-1 sm:grid-cols-2 gap-2'>
+                          {
+                              ["360 camera",  "Bluetooth",  "GPS", "Heated seats", "Rear View Mirror"].map((item)=>(
+                                <li key={item} className='flex items-center text-gray-500'>
+                                  <img src={assets.check_icon} className='h-4 mr-2' alt="" />
+                                  {item}
+                                </li>
+                              ))
+                          }
+                         </ul>
+                        </div>
+                 </div>
+             </div>
+
+             {/* Right: Booking Form */}
+             <form></form>
+         </div>
+
     </div>
-  ) : <p>Loading...</p>
+  ) : <Loader />
 }
 
 export default CarDetails
