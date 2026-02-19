8248567deabadca2ffbf8cedcbfc210c5b596907
Talha <your-github-email@gmail.com>
2026-02-19 16:30:13 +0500

    commit

--- src/pages/Cars.jsx
+++ src/pages/Cars.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import Title from '../components/Title'
 import { assets } from '../assets/assets'
 import CarCard from '../components/CarCard'
